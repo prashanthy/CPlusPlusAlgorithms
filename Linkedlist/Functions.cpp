5db437796d3f9d4a625ac3bafdeda9ff3ae847a0
cynosureofhyd <tomu646@gmail.com>
2014-01-10 22:20:53 -0700

    First Commit of linked list

---
+++ Linkedlist/Functions.cpp
@@ -0,0 +1,45 @@
+struct node
+{
+	node *leftNode;
+	node * rightNode;
+	int data=data;
+}
+
+static bool lookup(node *findNode, int find_value)
+{
+if (findNode==null)
+	return false;
+else 
+	if (find_value==findNode->data)
+		return true;
+	else 
+		if (target<findNode->data)
+			return (findNode->left,find_value);
+		else 
+			return (findNode->right,find_value);
+
+}
+
+
+node insert(node *root,node *newNode)
+{
+	if (root==NULL)
+		root=newNode;
+	else
+		if 
+}
+
+
+int main()
+{
+
+	return 0;
+}
+
+int length(struct *node)
+{
+
+	int count = 0;
+
+	return count;
+}
